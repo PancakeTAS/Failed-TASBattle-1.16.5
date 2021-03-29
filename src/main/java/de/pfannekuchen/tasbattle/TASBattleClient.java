3912398c69dd9a81758246c0305f5b0ebc4fc168
Pfannekuchen <gamesupport@mgnet.work>
2021-03-29 21:45:41 +0200

    Initial commit

---
+++ src/main/java/de/pfannekuchen/tasbattle/TASBattleClient.java
@@ -0,0 +1,12 @@
+package de.pfannekuchen.tasbattle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TASBattleClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+}
